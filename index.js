005cdb7377845097f4ef5fe3b9e15e8dfe6734df
unknown <jansen.cara@sp.senai.br>
2021-09-15 22:41:45 -0300

    Atualizei o index.js

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-let NomeProrpio = "Jansen"
-console.log(NomeProrpio)
-const NomeProrpioFixo = "Jansen 2"
-console.log(NomeProrpioFixo)
+const ListaDePecas = ['Peca 22', 'Ar','ABCDEF']
+if (ListaDePecas.length <= 10) {
+    console.log('As peças podem ser cadastradas');
+}
+console.log('Quantidade de caracteres');
+for (let index = 0; index < ListaDePecas.length; index ++) {
+    var PecaAtual = ListaDePecas[index];
+    if (PecaAtual.length < 3) {
+        console.log(PecaAtual + ': esta peça possui nome "inferior" a 3 caracteres!')
+
+    } else {
+        console.log(PecaAtual + ": esta peça poderá ser cadastrada")
+    }
+}
+console.log('Peso da Peça:')
+var PesoDaPeca = 50;
+if (PesoDaPeca >=100) {
+    console.log('Peso suficiente.')
+} else {
+    console.log('Peso insuficiente')
+}
